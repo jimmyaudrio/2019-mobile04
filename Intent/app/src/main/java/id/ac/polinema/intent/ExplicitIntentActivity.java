3bbc301d59e2333dbd577e46256daf02b19ea2c2
jimmyaudrio <jimmyaudrio16111999@gmail.com>
2019-09-25 22:54:34 +0700

    kurang bundle dan terakhir

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -6,13 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    EditText name;
+    TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        name = findViewById(R.id.input_name);
+        output = findViewById(R.id.text_output);
     }
 
+    public void handleButton(View view) {
+        String nama = name.getText().toString();
+        if(nama.equalsIgnoreCase("")){
+            Toast.makeText(this, "Harus di isi", Toast.LENGTH_SHORT).show();
+        }
+        output.setText("Hello "+nama+" Congratulation");
+    }
 }
